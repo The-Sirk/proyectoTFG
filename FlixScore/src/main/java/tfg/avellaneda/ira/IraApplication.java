3763db58ce5b813926a0bf00115d83fc9d87c199
RicardoHB996 <ricardo.he.bl@gmail.com>
2025-10-20 18:03:55 +0200

    Me daba error al abrir el proyecto y actualizo lo siguiente. Cambio de nombre de la clase IeaApplication a IraApplication mismo nombre que el archivo. Cambio de nombre archivo java BuscaPelicula a BuscaPeliculaPorNombre.

--- FlixScore/src/main/java/tfg/avellaneda/ira/IraApplication.java
+++ FlixScore/src/main/java/tfg/avellaneda/ira/IraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IeaApplication {
+public class IraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IeaApplication.class, args);
+		SpringApplication.run(IraApplication.class, args);
 	}
 
 }
