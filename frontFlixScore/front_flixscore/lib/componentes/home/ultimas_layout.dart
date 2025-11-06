583cf700c0686aab3392fce3235581a41a9ac921
Opa Hostil <ivaldeolivas@outlook.es>
2025-11-06 15:31:53 +0100

    limpiamos importaciones innecesarias

--- frontFlixScore/front_flixscore/lib/componentes/home/ultimas_layout.dart
+++ frontFlixScore/front_flixscore/lib/componentes/home/ultimas_layout.dart
@@ -1,4 +1,4 @@
-import 'package:flixscore/componentes/home/card_pelicula.dart';
+
 import 'package:flutter/material.dart';
 class UltimasLayout extends StatefulWidget {
   const UltimasLayout({super.key});
